ea08a279aef1032cc21beae4d4dbac3dbd50e01b
Thomas Getgood <thomas.getgood@gmail.com>
2015-05-27 12:05:38 -0400

    Separated URL generator from tests.

--- test/formic/core_test.clj
+++ test/formic/core_test.clj
@@ -3,38 +3,15 @@
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :refer [for-all]]
+            [formic.gens :refer [url]]
 
+            [org.httpkit.client :as http]
             [org.httpkit.fake :refer [with-fake-http]]
             [clojure.core.async :as async :refer [put! chan]]
 
             [clojure.string :as string] 
 
             [formic.core :refer :all]))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;;;; URL generation
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(def prot
-  (gen/elements ["http" "https"]))
-
-(def non-empty
-  (gen/such-that not-empty gen/string-alphanumeric))
-
-(def domain
-  (gen/fmap (fn [parts] (string/join "." parts))
-            (gen/such-that #(> (count %) 1) non-empty)))
-
-(def path
-  (gen/fmap (fn [parts] (str "/" (string/join "/" parts)))
-            (gen/vector
-             non-empty)))
-
-(def url
-  (gen/fmap (fn [[prot dom path]]
-              (str prot "://" dom path))
-            (gen/tuple prot domain path)))
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;; Network generation
---
+++ test/formic/gens.clj
@@ -0,0 +1,32 @@
+(ns formic.gens
+  (:require [clojure.test.check.clojure-test :refer [defspec]]
+            [clojure.test.check.generators :as gen]
+            [clojure.test.check.properties :refer [for-all]]
+
+            [clojure.string :as string]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;; URL generation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def prot
+  (gen/elements ["http" "https"]))
+
+(def non-empty
+  (gen/such-that not-empty gen/string-alphanumeric))
+
+(def domain
+  (gen/fmap (fn [parts] (string/join "." parts))
+            (gen/such-that #(> (count %) 1) non-empty)))
+
+(def path
+  (gen/fmap (fn [parts] (str "/" (string/join "/" parts)))
+            (gen/vector
+             non-empty)))
+
+(def url
+  (gen/fmap (fn [[prot dom path]]
+              (str prot "://" dom path))
+            (gen/tuple prot domain path)))
+
+
